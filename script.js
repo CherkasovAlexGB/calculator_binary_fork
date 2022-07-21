6779f6d7ceadd6d5f8b72d793ea6fcc3d594d960
CherkasovAlexGB <cherkasov_alex75@mail.ru>
2022-07-21 16:02:40 +0700

    Добавили файл index.html внесли изменения в код

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 let num
 num=prompt('Десятичное число: ')
 let arrey=[]
+let string1
+let string2
 //calc(num)
 
-if (Number.isInteger(num)){
+if (!Number.isInteger(+num)){
+	let n = 5; //точность. Сколько брать чисел после запятой
+	fractional = Math.floor((num % 1) * Math.pow(10, n));
+	fractional = '0' + '.' + fractional
+	calc_multiplication(+fractional)
+	num = Math.floor(num)
 	calc_division(num)
 }
 else{
-	num = Math.floor(num)
-	let n = 5; //точность. Сколько брать чисел после запятой
-	fractional = Math.floor((num % 1) * Math.pow(10, n));
+	// num = Math.floor(num)
+	// let n = 5; //точность. Сколько брать чисел после запятой
+	// fractional = Math.floor((num % 1) * Math.pow(10, n));
 	calc_division(num)
-	calc_multiplication(fractional)
+	//calc_multiplication(fractional)
 }
 
 function calc_division(num){
@@ -24,20 +31,33 @@ function calc_division(num){
 	}
 	arrey.push(1)
 	arrey.reverse()
-	calc_res(arrey)
+	arrey_string(arrey)
+	string1 = string
+	calc_res(string1)
 	// alert(arrey.join(' '))
 }
 
 function calc_multiplication(num){
 	let i = 0
-	for (n = num; n <= 0; i++){
+	for (n = num; n < 1; i++){
 		n = num *2 
 		num = n
 		arrey[i] = Math.floor(n)
+		console.log(arrey)
 	}
-	calc_res(arrey)
+	arrey_string(arrey)
+	string2 = string
+	calc_res(string2)
 }
 
-function calc_res(arrey){
-	alert(arrey.join(' ') + ',' + arrey.join(' '))
+function calc_res(string1, string2) {
+	if (string2 === true){
+		alert(string1 + ',' + string2)
+	}
+	else alert(string1)
+	
+}
+
+function arrey_string(arrey){
+	return string = arrey.join(' ')
 }
