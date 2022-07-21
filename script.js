538407b03428fd1585e5102830fd9f4f411ff940
CherkasovAlexGB <cherkasov_alex75@mail.ru>
2022-07-21 12:47:14 +0700

    Создали функцию умножения и вывода

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ num=prompt('Десятичное число: ')
 let arrey=[]
 calc(num)
 
-function calc(num){
+function calc_division(num){
 	let i=0
-	for (n=num; n>=2;){
+	for (n=num; n>=2; i++){
 	n=Math.trunc(num/2)
 	arrey[i] = num - n*2
 	num=n
-	i++
+	//i++
 	}
+	calc_res(arrey)
+	// arrey.push(1)
+	// arrey.reverse()
+	// alert(arrey.join(' '))
+}
+
+function calc_multiplication(num){
+	let i = 0
+	for (n = num; n <= 0; i++){
+		n = num *2 
+		num = n
+		arrey[i] = Math.floor(n)
+	}
+}
+
+function calc_res(arrey){
 	arrey.push(1)
 	arrey.reverse()
 	alert(arrey.join(' '))
