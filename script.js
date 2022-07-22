839ffdb0a55c23d35575a9bd3cd91524a99dc0b6
CherkasovAlexGB <cherkasov_alex75@mail.ru>
2022-07-22 18:15:36 +0700

    Создали файл index.html

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let num
 num=prompt('Десятичное число: ')
-let arrey=[]
+let array=[]
 calc(num)
 
 function calc(num){
 	let i=0
 	for (n=num; n>=2;){
 	n=Math.trunc(num/2)
-	arrey[i] = num - n*2
+	array[i] = num - n*2
 	num=n
 	i++
 	}
-	arrey.push(1)
-	arrey.reverse()
-	alert(arrey.join(' '))
+	array.push(1)
+	array.reverse()
+	//alert(array.join(' '))
+	let  res = array.join(' ')
+	let print=document.querySelector('.print')
+	print.innerHTML = `${res}`
 }
