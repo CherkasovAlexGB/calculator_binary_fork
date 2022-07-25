c70ddfff6ec313386fd9e69072624e4d0b7631fd
CherkasovAlexGB <cherkasov_alex75@mail.ru>
2022-07-25 14:18:44 +0700

    Исправление названия функций

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let num
 num=prompt('Десятичное число: ')
 let array=[]
-let string1=[]
+let arr_string=[]
 //let string2
 let fractional
-let notation = 2
+let notation = 16
 
 // Checking "integer or not" (проверка "целое число или нет")
 if (!Number.isInteger(+num)){
 	fractional = (num % 1) 
 	num = Math.floor(num)
-	string1[0] = calc_division(num)
+	arr_string[0] = calcDivision(num)
 	//console.log(string1[0])
-	string1[1] = calc_multiplication(fractional)
+	arr_string[1] = calcMultiplication(fractional)
 	//calc_res(string1, string2)
-	calc_res(string1)
+	calcRes(arr_string)
 }
 else{
-	string1[0] = calc_division(num)
-	calc_res(string1)
+	arr_string[0] = calcDivision(num)
+	calcRes(arr_string)
 }
 
 // Division of a part of a up to a comma (деление части числа до запятой)
-function calc_division(num){
+function calcDivision(num){
 	let i=0, a
 	for (n=num; true; i++){
 	if (num<notation){
@@ -42,40 +42,13 @@ function calc_division(num){
 	}
 	//array.push(1)
 	array.reverse()
-	conversion_array_string(array)
+	conversionArrayStr(array)
 	return string
 }
 
-// function calc_division(num){
-// 	let i = 0, n, a
-// 	do {
-// 		//n = num 
-// 		n=Math.trunc(num/notation)
-// 		a = n*notation
-// 		if (n<notation){
-// 			a = 0
-// 		}
-// 		//a = n
-// 		array[i] = num - a
-// 		if (array[i] === num) break
-// 		num = n
-// 		if ((array[i]) >= 10){
-// 			array[i] = transformNumber(array[i])
-// 		}
-// 		//num = n
-// 		// if (n<notation){
-// 		// 	n=0
-// 		// }
-// 		i++
-// 	}while(true)
-// 	console.log(array)
-// 	array.reverse()
-// 	conversion_array_string(array)
-// 	return string
-// }
 
 // Translation of the part after the comma (перевод части после запятой)
-function calc_multiplication(num){
+function calcMultiplication(num){
 	let i = 0
 	let n=5
 	for (number = num; i<=10; i++){
@@ -84,12 +57,12 @@ function calc_multiplication(num){
 		num = (number % 1)
 		//console.log(num)
 	}
-	conversion_array_string(array)
+	conversionArrayStr(array)
 	return string
 }
 
 // Output on display (вывод на экран)
-function calc_res(array) {
+function calcRes(array) {
 	let res
 	array.forEach(function(item, i, array){
 		if (i <1){
@@ -108,7 +81,7 @@ function calc_res(array) {
 }
 
 // Convert array to string (конвертация массива в строку)
-function conversion_array_string(array){
+function conversionArrayStr(array){
 	string = array.join('')
 	return string
 }
